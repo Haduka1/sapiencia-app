c5cfc925ef2a2456bdaf28908e198a1b20181ab3
Haduka1 <157337576+Haduka1@users.noreply.github.com>
2025-05-21 03:49:54 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('root').innerHTML = `
-  <div class="container">
-    <img src="logo.jpg" alt="Logo Sapiencia" />
-    <h1>Bem-vindo ao Sapiencia</h1>
-    <p>O app de estudos está ativo e rodando corretamente.</p>
-  </div>
-`;
